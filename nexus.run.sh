d6dbee24366400fb3985b65bb4ae9b41608d6b93
Thorben Stangenberg <thorben@stangenberg.ch>
2015-12-08 20:04:00 +0100

    removed extra java heap config. Its default is in the JAVA_OPTS environment variable.

--- nexus.run.sh
+++ nexus.run.sh
@@ -2,7 +2,6 @@
 # runs the nexus
 
 cd /opt/sonatype/nexus && exec java \
-  -Xms${MIN_HEAP} -Xmx${MAX_HEAP} \
   ${JAVA_OPTS} \
   -cp conf/:`(echo lib/*.jar) | sed -e "s/ /:/g"` \
   org.sonatype.nexus.bootstrap.Launcher conf/jetty.xml conf/jetty-requestlog.xml
